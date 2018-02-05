df7602e062a69125d70734d1ab2ddf50bf6235cd
Anmol varma <anmol_varma@github.com>
2018-02-06 00:21:39 +0530

    the api returns an object with keys as author names and values as an array of their books along with there name and rating

--- routes/getRequest.js
+++ routes/getRequest.js
@@ -11,19 +11,31 @@ module.exports = [
         .then(response => response.data)
         .then((list) => {
           booklist = list.books;
-          list.books.map(eachBook => axios.get(`https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/findBookById/${eachBook.id}`)
+          return list.books.map(eachBook => axios.get(`https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/findBookById/${eachBook.id}`)
             .then(response => response.data));
         })
         .then(list => Promise.all(list))
-        .then((rateList) => {
-          booklist.map((entry, index) => {
-            const newEntry = entry;
-            newEntry.rating = rateList[index];
-            return newEntry;
-          });
+        .then(rateList => booklist.map((entry, index) => {
+          const newEntry = entry;
+          newEntry.rating = rateList[index].rating;
+          return newEntry;
+        }))
+        .then(listWithRating => listWithRating.reduce((result, item) => {
+          console.log(item.Author);
+          if (!result[item.Author]) {
+            result[item.Author] = new Array(item);
+          } else {
+            result[item.Author].push(item);
+          }
+          return result;
+        }, {}))
+        .then((result) => {
+          console.log(result);
+          reply(result);
         })
         .catch((error) => {
           console.log(error);
+          reply(false);
         });
     },
   }];
