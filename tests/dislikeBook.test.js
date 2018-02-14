1112443e99dc4a98a8330f8efae5364caf7412e5
Anmol varma <anmol_varma@github.com>
2018-02-15 02:13:25 +0530

    All test api tested

--- tests/dislikeBook.test.js
+++ tests/dislikeBook.test.js
@@ -22,3 +22,26 @@ describe('Server for valid bookid test', () => {
     });
   });
 });
+
+describe('Server for invalid bookid test', () => {
+  test('responds with resource not found', (done) => {
+    const options = {
+      method: 'GET',
+      url: '/book/dislike/100',
+    };
+    Server.inject(options, (response) => {
+      expect(response.result.statusCode).toBe(404);
+      done();
+    });
+  });
+  test('responds with message for upsert', (done) => {
+    const options = {
+      method: 'GET',
+      url: '/book/dislike/100',
+    };
+    Server.inject(options, (response) => {
+      expect(response.result.message).toBe('Bookid doesnot exit.');
+      done();
+    });
+  });
+});
